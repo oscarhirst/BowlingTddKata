a0749e2017b873dc06b304467afe179c31c74b07
oscarhirst <10322447+oscarhirst@users.noreply.github.com>
2020-02-12 11:09:04 +0000

    scores strikes

--- BowlingTddKata/ScoringEngine.cs
+++ BowlingTddKata/ScoringEngine.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Corsham Science. All rights reserved.
 // </copyright>
 // sln based on exercise http://www.peterprovost.org/blog/2012/05/02/kata-the-only-way-to-learn-tdd/
+// using World Bowling Scoring
 
 namespace Bowling
 {
@@ -9,9 +10,14 @@ namespace Bowling
 
     public class ScoringEngine
     {
-        public int ScoreRound(int pinsKnocked)
+        public int ScoreRoll(int pinsKnocked)
         {
-            return default;
+            if (pinsKnocked > 10)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return pinsKnocked == 10 ? 30 : pinsKnocked;
         }
     }
 }
--- BowlingTests/ScoringEngineTests.cs
+++ BowlingTests/ScoringEngineTests.cs
@@ -4,6 +4,7 @@
 
 namespace Bowling.Tests
 {
+    using System;
     using NUnit.Framework;
 
     [TestFixture]
@@ -18,9 +19,31 @@ namespace Bowling.Tests
         }
 
         [Test]
-        public void ScoreRound_Given0Knocked_Returns0()
+        public void ScoreRoll_Given0Knocked_Returns0()
         {
-            Assert.AreEqual(0, _engine.ScoreRound(0));
+            Assert.AreEqual(0, _engine.ScoreRoll(0));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(6)]
+        [TestCase(8)]
+        public void ScoreRoll_GivenLessThan10Knocked_ReturnsAPointPerPinKnocked(int pinsKnocked)
+        {
+            Assert.AreEqual(pinsKnocked, _engine.ScoreRoll(pinsKnocked));
+        }
+
+        [TestCase(11)]
+        [TestCase(1010)]
+        public void ScoreRoll_GivenGreaterThan10Knocked_ThrowsInvalidOperationException(int pinsKnocked)
+        {
+            Assert.Catch(typeof(InvalidOperationException), () => { _engine.ScoreRoll(pinsKnocked); });
+        }
+
+        [Test]
+        public void ScoreRoll_GivenStrike_Returns30()
+        {
+            Assert.AreEqual(30, _engine.ScoreRoll(10));
         }
     }
 }
